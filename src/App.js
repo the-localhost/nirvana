e8a3eed6f2873dbef47af8021fc799a9fe693be1
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-04 20:43:21 +0530

    imported Login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey, finally working on it again!!</h1>
+      {/* Spotify Logo */}
+      {/* Spotify Login Button */}
+      <Login />
     </div>
   );
 }
