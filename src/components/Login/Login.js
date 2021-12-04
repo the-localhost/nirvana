606be70d6eb15f19df7e87f8194216cd66f43403
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-04 20:37:41 +0530

    Created basic login component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,13 @@
+ import React from 'react'
+ 
+ function Login() {
+     return (
+         <div>
+             {/* Spotify Logo */}
+             {/* Spotify Login Button*/}
+         </div>
+     )
+ }
+ 
+ export default Login
+ 
