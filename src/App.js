ebad0d2e6f2933aef68128167d7390c56b0b3125
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-05 12:22:51 +0530

    token state created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './components/Login/Login';
 import { getHashObjFromUrl } from './process/auth';
 
 function App() {
+  const [token, setToken] = useState(null); 
 
   // code to run below code whenever App component is rendered
   useEffect(()=> {
     const hashObj = getHashObjFromUrl();
     window.location.hash = "";    // to hide token details from user
-    const token = hashObj.access_token;
-    console.log('MY TOKEN', token);
+    const access_token = hashObj.access_token;
+    if(access_token){
+      setToken(access_token);
+    }
   },[]);
 
   return (
