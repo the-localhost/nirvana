4f41bf2bda35832598b8a4c8917dfc1a7c54cd32
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-05 12:04:40 +0530

    useEffect to get access token

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import Login from './components/Login/Login';
+import { getHashObjFromUrl } from './process/auth';
 
 function App() {
+
+  // code to run below code whenever App component is rendered
+  useEffect(()=> {
+    const hashObj = getHashObjFromUrl();
+    window.location.hash = "";    // to hide token details from user
+    const token = hashObj.access_token;
+    console.log('MY TOKEN', token);
+  },[]);
+
   return (
     <div className="app">
       <Login />
