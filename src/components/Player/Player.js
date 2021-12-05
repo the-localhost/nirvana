346dde8c2884c128aec020adb9e519e972f46b69
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-05 13:04:17 +0530

    created Player component to be shown after login

---
+++ src/components/Player/Player.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Player() {
+    return (
+        <div>
+            <h1>You're logged in successfully!</h1>
+        </div>
+    )
+}
+
+export default Player
