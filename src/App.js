687c387dda0e09b588c14e00d67e9f669f3513e2
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-05 12:35:30 +0530

    imported and created instance of spotify web api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './components/Login/Login';
 import { getHashObjFromUrl } from './process/auth';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null); 
