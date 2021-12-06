b8e1accc826061251d2cf307c568d41b5bee67fa
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-07 00:29:06 +0530

    created body component

---
+++ src/components/Body/Body.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+    return (
+        <div>
+            <h1>Heyy, this is Body</h1>
+        </div>
+    )
+}
+
+export default Body
