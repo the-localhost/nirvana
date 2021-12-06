b2c85a14c0be5581ba254dc82bb58bf214527f30
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-07 00:39:38 +0530

    changed sidebar component content

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './SideBar.css'
 
 function SideBar() {
     return (
-        <div>
-            
+        <div className="sidebar">
+            <h1>Heyy, this is SideBar </h1>
         </div>
     )
 }
