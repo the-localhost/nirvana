9e8d7ef4c0d822e0cf7c7a2854dac10059828542
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-07 00:58:31 +0530

    added the spotify logo

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -4,7 +4,10 @@ import './SideBar.css'
 function SideBar() {
     return (
         <div className="sidebar">
-            <h1>Heyy, this is SideBar </h1>
+            <img className="sidebar_logo"
+                src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
+                alt="Spotify" 
+            />
         </div>
     )
 }
