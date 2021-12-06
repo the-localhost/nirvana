484901169476523cf14770fbf87fc35138aac839
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-06 22:58:13 +0530

    created hook to get value of StateContext

--- src/components/StateProvider.js
+++ src/components/StateProvider.js
@@ -2,6 +2,11 @@ import React, {createContext, useContext, useReducer} from 'react';
 
 const StateContext = createContext();
 
+// this is a custom hook to get the context value
+// getStateContextValue is more apt name, but we
+// start hook's name with 'use', hence below name
+const useStateContextValue = () => useContext(StateContext);
+
 // this is the datalayer to provide state to all components
 // along with the ability to make changes
 function StateProvider({initialState, reducer, children}) {
@@ -13,4 +18,4 @@ function StateProvider({initialState, reducer, children}) {
 }
 
 export default StateProvider;
-export {StateContext};
+export {StateContext, useStateContextValue};
