c24d3e797c9321ba713ba37664ccb7e4013d7bbb
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-06 23:23:59 +0530

    called the custom hook to use StateContext

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Login from './components/Login/Login';
 import Player from './components/Player/Player';
 import { getHashObjFromUrl } from './process/auth';
 import SpotifyWebApi from 'spotify-web-api-js';
+import { useStateContextValue } from './components/StateProvider';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null); 
 
+  // type of StateContext value === return type of useReducer()
+  const [{}, dispatch] = useStateContextValue();
+
   // code to run below code whenever App component is rendered
   useEffect(()=> {
     const hashObj = getHashObjFromUrl();
-    window.location.hash = "";    // to hide token details from user
+    window.location.hash = '';    // to hide token details from user
     const access_token = hashObj.access_token;
 
     if(access_token){
