6d22a1fcaad618a1b535c02cd5e3d294b9273db3
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-11 19:32:08 +0530

    import csss & assigned className

--- src/components/SidebarNav/SidebarNav.js
+++ src/components/SidebarNav/SidebarNav.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './SidebarNav.css'
 
 // this is the navigation option in sidebar (like Home, Library, etc.)
 // it has an icon and a name
 function SidebarNav({Icon, title}) {
     return (
-        <div>
+        <div className="sidebarNav">
             <p>{title}</p>
         </div>
     )
