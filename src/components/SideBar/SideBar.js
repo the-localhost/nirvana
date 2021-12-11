e40c78babf458d5805039e2596fe617b596dcf1c
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-11 19:22:49 +0530

    added SidebarNav components

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -10,7 +10,9 @@ function SideBar() {
                 src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
                 alt="Spotify" 
             />
-            <SidebarNav />
+            <SidebarNav title="Home"/>
+            <SidebarNav title="Search"/>
+            <SidebarNav title="Your Library"/>
         </div>
     )
 }
