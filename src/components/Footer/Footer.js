108584a20eb98c576e391d43a731cf951c8f0439
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-12 00:40:01 +0530

    imported and added icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import './Footer.css'
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import RepeatIcon from '@mui/icons-material/Repeat';
 
 function Footer() {
     return (
         <div className="footer">
-            <h1> Heyy, this is footer</h1>
+            <div className="footer__left">
+                <h1>Footer left</h1>
+            </div>
+            <div className="footer__center">
+                <ShuffleIcon className="footer__green" />
+                <SkipPreviousIcon className="footer__icon" />
+                <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+                <SkipNextIcon className="footer__icon" />
+                <RepeatIcon className="footer__green" />
+            </div>
+            <div className="footer__right">
+                <h1>Footer right</h1>
+            </div>
         </div>
     )
 }
