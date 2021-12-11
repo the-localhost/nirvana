1b76edfcbe4dbfbd5e9131460844a10816a02207
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-11 23:58:08 +0530

    added comments

--- src/components/SidebarNav/SidebarNav.js
+++ src/components/SidebarNav/SidebarNav.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './SidebarNav.css'
 
-// this is the navigation option in sidebar (like Home, Library, etc.)
-// it has an icon and a name
+/** SidebarNav is used for upper navigation like Home, Search, etc
+    as well as to navigate to the playlists. In case of upper navigation,
+    it also has an Icon. */
 function SidebarNav({Icon, title}) {
     return (
         <div className="sidebarNav">
