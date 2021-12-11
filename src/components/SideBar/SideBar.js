1fc81874a17896a5a7e2271d4e28dca3f5e3a3a8
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-11 23:55:07 +0530

    added playlists after pulling it from api

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -5,21 +5,28 @@ import './SideBar.css'
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import { useStateContextValue } from '../StateProvider';
 
 // this contains our icon, playlists, & navigation options
 function SideBar() {
+    const [{playlists}, dispatch] = useStateContextValue();
     return (
         <div className="sidebar">
             <img className="sidebar__logo"
                 src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
                 alt="Spotify" 
             />
+            {/* Upper navigation (Home, Search, Library) */}
             <SidebarNav Icon={HomeIcon} title="Home"/>
             <SidebarNav Icon={SearchIcon} title="Search"/>
             <SidebarNav Icon={LibraryMusicIcon} title="Your Library"/>
             <br />
             <strong className="sidebar__title">PLAYLISTS</strong>
             <hr />
+            {/* Navigate to the playlists. */}
+            {playlists?.items?.map((playlist, id) => (
+                <SidebarNav key={id} title={playlist.name} />
+            ))}
         </div>
     )
 }
