6e1eb2134a9bebbe2e9d19080aa184e57210647d
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-11 19:58:53 +0530

    imported icons from MUI

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import SidebarNav from '../SidebarNav/SidebarNav'
 import './SideBar.css'
+// importing material-UI icons
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 
 // this contains our icon, playlists, & navigation options
 function SideBar() {
@@ -10,9 +14,9 @@ function SideBar() {
                 src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
                 alt="Spotify" 
             />
-            <SidebarNav title="Home"/>
-            <SidebarNav title="Search"/>
-            <SidebarNav title="Your Library"/>
+            <SidebarNav Icon={HomeIcon} title="Home"/>
+            <SidebarNav Icon={SearchIcon} title="Search"/>
+            <SidebarNav Icon={LibraryMusicIcon} title="Your Library"/>
         </div>
     )
 }
