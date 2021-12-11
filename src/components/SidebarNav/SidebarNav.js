45e7d987ee5df3a0ad142349fa5c1ac769f720d9
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-11 19:15:39 +0530

    created SidebarNav component

---
+++ src/components/SidebarNav/SidebarNav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// this is the navigation option in sidebar (like Home, Library, etc.)
+// it has an icon and a name
+function SidebarNav() {
+    return (
+        <div>
+            <p>Let's go!</p>
+        </div>
+    )
+}
+
+export default SidebarNav
