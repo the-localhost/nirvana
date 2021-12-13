f08457c2f108d3ca5cc08f2f2d7cd99f9fdeb7bb
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-13 18:50:04 +0530

    created search option

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header() {
     return (
         <div>
             <div className="header__left">
-
+                <SearchIcon />
+                <input 
+                    placeholder="Search for Artists, Songs, or Podcasts"
+                    type="text"
+                />
             </div>
             <div className="header__right">
-                
+
             </div>
         </div>
     )
