71e1d97af6bc6f0836fe7d927bbf5afc5426eb75
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-13 22:32:34 +0530

    added weekly_dicover

--- src/process/reducer.js
+++ src/process/reducer.js
@@ -1,9 +1,10 @@
 const initialState = {
-    token: 'BQCLcQvfonCvr8PvumtBa2pXKsC0QKd25rINIiziV2Tzc4KvUUD1pItpyd1N88AGrbUgJ4qq02yak_wjCpBapcZeA8Y',
+    // token: 'BQCLcQvfonCvr8PvumtBa2pXKsC0QKd25rINIiziV2Tzc4KvUUD1pItpyd1N88AGrbUgJ4qq02yak_wjCpBapcZeA8Y',
     user: null,
     item: null,
     playing: false,
     playlists:[],
+    dicover_weekly:[],
 }
 
 const reducer = (state=initialState, action) => {
@@ -22,6 +23,11 @@ const reducer = (state=initialState, action) => {
             return {
                 ...state,
                 playlists: action.playlists
+            }
+        case 'SET_DISCOVER_WEEKLY':
+            return {
+                ...state,
+                discover_weekly: action.playlist
             }
         default:
             return state;
