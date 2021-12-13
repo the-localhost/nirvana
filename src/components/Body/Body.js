971950d1baeda2844e326b414f0dd58db86a8aab
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-13 20:56:39 +0530

    created body__info container

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -6,6 +6,14 @@ function Body() {
     return (
         <div className="body">
             <Header />
+            <div className="body__info">
+                <img src="https://newjams-images.scdn.co/image/ab676477000033ad/dt/v3/discover-weekly/wxbtkeD8kGNV7uDBykUPTEutmT9zPs5qC_Ex1nnxaOF9SybbfeuiDvuyvM835BtbImxfo1nV8mYzUjYfSs-rISFQV2zr9KxpQBfBMeh-Mds=/NzU6NzM6OTBUMzItMjEtMQ==" alt="" />
+                <div className="body__infoText">
+                    <strong>PLAYLIST</strong>
+                    <h2>Discover Weekly</h2>
+                    <p>decription...</p>
+                </div>
+            </div>
         </div>
     )
 }
