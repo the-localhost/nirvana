ca59d83633e7d582dd3c07519f0f2711134c7a9e
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-13 22:33:19 +0530

    pulled discover_weekly from context

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import Header from '../Header/Header'
+import { useStateContextValue } from '../StateProvider'
 import './Body.css'
 
 function Body() {
+    const [{discover_weekly}, dispatch] = useStateContextValue();
+
     return (
         <div className="body">
             <Header />
             <div className="body__info">
-                <img src="https://newjams-images.scdn.co/image/ab676477000033ad/dt/v3/discover-weekly/wxbtkeD8kGNV7uDBykUPTEutmT9zPs5qC_Ex1nnxaOF9SybbfeuiDvuyvM835BtbImxfo1nV8mYzUjYfSs-rISFQV2zr9KxpQBfBMeh-Mds=/NzU6NzM6OTBUMzItMjEtMQ==" alt="" />
+                <img src={discover_weekly?.images[0]?.url} alt="" />
                 <div className="body__infoText">
                     <strong>PLAYLIST</strong>
                     <h2>Discover Weekly</h2>
-                    <p>decription...</p>
+                    <p>{discover_weekly?.description}</p>
                 </div>
             </div>
         </div>
