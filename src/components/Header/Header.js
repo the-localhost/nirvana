1f1d0bd846b95c940419b48b58a9dda89a2f682b
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-13 15:59:45 +0530

    created and divided header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <div className="header__left">
+
+            </div>
+            <div className="header__right">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header
