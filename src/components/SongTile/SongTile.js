21372162018fec3207536705009c15662bc0cd9d
the-localhost <gagan.agarwal.met17@itbhu.ac.in>
2021-12-14 09:29:21 +0530

    added image, name & artists

--- src/components/SongTile/SongTile.js
+++ src/components/SongTile/SongTile.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
-function SongTile() {
+function SongTile( {track} ) {
     return (
-        <div>
-            <h4>I'm the song tile.</h4>
+        <div className = 'songTile'>
+            {/* Song album pictue */}
+            <img className='songTile__album'
+                src={track.album.images[0].url} 
+                alt="" 
+            />
+            {/*  */}
+            <div className="songTile__info"></div>
+
+            {/* Artists */}
+            <p>
+                {track.artists.map((artist)=> artist.name).join(", ")} {" "} - {" "} {track.album.name}
+            </p>
         </div>
     )
 }
